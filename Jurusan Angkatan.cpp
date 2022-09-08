e0bd6bd7d81ffba1b8ae2b9285ba943a0ee2e830
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2022-09-08 10:06:49 +0700

    Add files via upload

---
+++ Jurusan Angkatan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string angkatan;
+	string jurusan;
+	
+	cout<<"Angkatan : ";
+	getline(cin,angkatan);
+	cout<<"Jurusan : ";
+	getline(cin,jurusan);
+
+	return 0;
+}
