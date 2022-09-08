94f4a02d33a46a9c5a0c29997665ad09d54260b5
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2022-09-08 09:47:26 +0700

    Add files via upload

---
+++ keliling luas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int sisi,luas,keliling;
+
+	
+	cout<<"Masukkan Sisi Persegi : ";
+	cin>>sisi;
+	
+	luas = sisi*sisi;
+	cout<<"Luas Persegi Adalah "<<luas<<endl;
+	
+	keliling = 4*sisi;
+	cout<<"Keliling Persegi Adalah "<<keliling;
+	return 0;
+}
+
